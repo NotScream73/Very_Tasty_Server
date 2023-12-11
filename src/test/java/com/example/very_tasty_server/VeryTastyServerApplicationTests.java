155ed0824cb3ba5c7c3f215a0aed4058022b5c48
NotScream73 <notscreamplay73@gmail.com>
2023-12-11 15:14:14 +0400

    first commit

---
+++ src/test/java/com/example/very_tasty_server/VeryTastyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.very_tasty_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeryTastyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
